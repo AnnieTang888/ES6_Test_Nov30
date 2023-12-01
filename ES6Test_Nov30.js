fd3104891ddbb29bce4f9e5760b4704457c40277
Annie <annietang.california@gmail.com>
2023-11-30 22:34:42 -0800

    my changes

--- ES6Test_Nov30.js
+++ ES6Test_Nov30.js
@@ -77,6 +77,8 @@ fset.clear();
 // displaying the size of the set after clearing
 console.log("size of the set after clearing:", fset.size); // output: size of the set after clearing: 0
 
+//------------------------------------------------------------------------------------------------------------------------
+
 
 //3. Create a promise object that get resloved after two seconds and rejected after three. Also it returns five ES6 features on resolved
 // Creating a Promise
@@ -110,6 +112,8 @@ myPromise
         console.error(error.message); // This will be called if the promise is rejected
     });
 
+ //-------------------------------------------------------------------------------------------------------------------------------
+
 //4. Use the spread and rest operator to create a function which can multiple numbers from 1...n (n is the number of choice)
 //   also need to print students of the session using same example
 
@@ -135,6 +139,7 @@ function printStudents(session, ...students) {
 const jsSessionStudents = ['Annie', 'John', 'Joy', 'katie'];
 printStudents('JavaScript', ...jsSessionStudents);
 
+//-------------------------------------------------------------------------------------------------------------------------------------------------
 
 //5. Print the last name through destructuring and add a contact number:9119119110 as well
 
@@ -152,6 +157,8 @@ const { userDetails: { last }, contactNumber = "9119119110" } = person;
 console.log("Last Name:", last);
 console.log("Contact Number:", contactNumber);
 
+//---------------------------------------------------------------------------------------------------------------------------------------------------
+
 //6. Give me an example of const data manipulation
 const trainee = {
     name: "Annie",
@@ -163,6 +170,8 @@ trainee.ID = 666;
 trainee.address = "San Jose";
 
 console.log(trainee);
+
+//-----------------------------------------------------------------------------------------------------------------------------------------------------
 
 //7. What is the difference between for-of and for-in show with examples
 //for-of
@@ -187,6 +196,8 @@ for (const key in student) {
 //forin: iterates over the string properties of an object.
 //forof: iterates over values of the iterable object.
 
+//-------------------------------------------------------------------------------------------------------------------------------------------------------
+
 //8. Give me an example of bind and write its usage, comparison with arrow function
 function course(name) {
     console.log(`Hi, ${name}!  ${this.name} is very interesting.`);
@@ -203,6 +214,8 @@ const bindsubjectcourse = course.bind(subject);
 // Calling the bound function
 bindsubjectcourse('Annie');
 // Output: Hello, Alice! My name is John.
+
+//------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 //9. Create an example showing usage of event loop in concurrent execution cycle
 // Function to simulate an asynchronous operation
@@ -231,8 +244,9 @@ async function concurrentExecutionCycle() {
 
     console.log('Complete');
 }
-
 concurrentExecutionCycle();
+
+//----------------------------------------------------------------------------------------------------------------------------------------------
 
 //10. create an example showing usage of short hand, default param and template literal.
 function cat(name = 'Fat', age = 2, food = 'fish') {
